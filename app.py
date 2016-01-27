6e801a3bb290874e3b544dbe376095e2e45f2cf3
mjp2220 <mjp2220@columbia.edu>
2016-01-26 22:12:32 -0500

    Adding code for level 4

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, jsonify, render_template, request
 import requests
+from flask.ext.mongoengine import MongoEngine
 
 app = Flask(__name__)
 app.config["DEBUG"] = True  # Only include this while you are testing your app
+app.config['MONGODB_SETTINGS'] = { 'db': 'books' }
+
+db = MongoEngine(app)
+
+class FavoriteBook(db.Document):
+    author = db.StringField(required=True)
+    title = db.StringField(required=True)
+    link = db.StringField(required=True)
 
 @app.route("/")
 def hello():
-    return render_template("hello.html")
+  return render_template("hello.html")
 
 @app.route("/name")
 def name():
@@ -20,6 +29,19 @@ def search():
 		return render_template("results.html", api_data=response_dict)
 	else: # request.method == "GET"
 		return render_template("search.html")
+
+@app.route("/favorite/<id>")
+def favorite(id):
+  book_url = "https://www.googleapis.com/books/v1/volumes/" + id
+  book_dict = requests.get(book_url).json()
+  new_fav = FavoriteBook(author=book_dict["volumeInfo"]["authors"][0], title=book_dict["volumeInfo"]["title"], link=book_url)
+  new_fav.save()
+  return render_template("confirm.html", api_data=book_dict)
+
+@app.route("/favorites")
+def favorites():
+  favorites = FavoriteBook.objects()
+  return render_template("favorites.html", favorites=favorites)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
